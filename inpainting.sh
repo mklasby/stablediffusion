eef15f616ae1ff29be9c5f854f4a1205a9ac4810
Mike Lasby <mklasby@gmail.com>
2023-07-20 21:10:54 +0000

    updated req file and streamlit options

--- inpainting.sh
+++ inpainting.sh
@@ -7,10 +7,12 @@ export CKPT=./models/v2-1_512-ema-pruned.ckpt
 export SEED=42
 export STEPS=50
 export PORT=8000
+export MAX_MESSAGE_SIZE=400
 
 streamlit run \
  --server.port=${PORT} \
- --server.runOnSave True \
+ --server.runOnSave=True \
+ --server.maxMessageSize=${MAX_MESSAGE_SIZE} \
  scripts/streamlit/inpainting.py \
  configs/stable-diffusion/v2-inpainting-inference.yaml \
  ./models/512-inpainting-ema.ckpt
